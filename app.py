f45db28d3ca34b496b32bf836ce47aeac14e9333
ayushshrivastava9 <amitshrivastava13829@gmail.com>
2025-10-31 19:01:27 +0530

    Update and rename penal.py to app.py

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ def convo_server():
 # Application ko run karne ke liye
 if __name__ == '__main__':
     # Debug mode ON hai aur port 5000 par run hoga (0.0.0.0 for deployment)
+
     app.run(debug=True, host='0.0.0.0', port=5000)
